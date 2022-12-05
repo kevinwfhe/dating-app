e29c8e88b313489df7c8cb38bede36bdf9f6cb18
Nimish Aggarwal <nagga078@uottawa.ca>
2022-12-04 20:54:12 -0500

    added rds database

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'dating_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE' : 'django.db.backends.mysql',
+		'NAME' : 'datingapp',
+		'HOST' : 'datingdb.c57zeeoahvrx.us-east-1.rds.amazonaws.com',
+		'USER' : 'admin',
+		'PASSWORD' : 'Dating123',
+		'PORT' : '3306',
+	},
 }
+
 
 
 # Password validation
