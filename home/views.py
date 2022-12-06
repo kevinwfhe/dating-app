1021376996b9fe0704363501041bb09ada0234e2
kevin-7755 <valarmorghulis_kh@outlook.com>
2022-12-06 17:19:01 -0500

    match by interests

--- home/views.py
+++ home/views.py
@@ -13,8 +13,18 @@ def index(request):
         card_profiles = Profile.objects.filter(Q(looking_for=request.user.profile.gender) | Q(looking_for="BOTH")).filter(user__is_staff=False).exclude(user_id=request.user.id).all()[:10]
     else:
         card_profiles = Profile.objects.filter(Q(looking_for=request.user.profile.gender) | Q(looking_for="BOTH")).filter(user__is_staff=False).filter(gender=request.user.profile.looking_for).exclude(user_id=request.user.id).all()[:10]
-    
-    if card_profiles.count() == 0:
+
+    current_user_interests = request.user.profile.interests.all()
+    matched_profile = []
+    for profile in card_profiles:
+      match = False
+      for interest in profile.interests.all():
+        if interest in current_user_interests:
+          match = True
+      if match:
+        matched_profile.append(profile)
+
+    if matched_profile.__len__ == 0:
         card_profiles_exists = False
     else: 
         card_profiles_exists = True
@@ -22,7 +32,7 @@ def index(request):
     context = {
         'page_ref': 'home',
         'card_profiles_exists' : card_profiles_exists,
-        'card_profiles': card_profiles,
+        'card_profiles': matched_profile,
         'has_filled_profile': request.user.profile.has_filled_profile,
     }
     
